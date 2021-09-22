de12b23e5923cbcb3486192b53ad3bfb1cd91199
Sharanjeet Kaur <kaursharanjeet977@gmail.com>
2021-09-21 20:49:59 -0400

    added level-set gradient bounds

--- examples/examples_ls.cpp
+++ examples/examples_ls.cpp
@@ -2,7 +2,7 @@
 #include "/users/kaurs3/Sharan/Research/Spring_2020/algoim_fork/src/algoim_levelset.hpp"
 #include <iostream>
 // #include "algoim_quad.hpp"
-
+int ls = 2;
 /// this function constructs the normal vectors for given boundary points of level-set geometry
 template <int N>
 std::vector<TinyVector<double, N>> constructNormal(std::vector<TinyVector<double, N>> Xc)
@@ -37,6 +37,37 @@ std::vector<TinyVector<double, N>> constructNormal(std::vector<TinyVector<double
     }
     return nor;
 }
+template <int N>
+struct Ellipsoid
+{
+    template <typename T>
+    T operator()(const blitz::TinyVector<T, N> &x) const
+    {
+        return x(0) * x(0) + 16.0 * x(1) * x(1) - 16.0;
+    }
+
+    template <typename T>
+    blitz::TinyVector<T, N> grad(const blitz::TinyVector<T, N> &x) const
+    {
+        return blitz::TinyVector<T, N>(2.0 * x(0), 32.0 * x(1));
+    }
+};
+
+template <int N>
+struct Circle
+{
+    template <typename T>
+    T operator()(const blitz::TinyVector<T, N> &x) const
+    {
+        return ((x(0)-2.0) * (x(0) - 2.0)) + ((x(1)-2.0)* (x(1)-2.0)) - 1.0;
+    }
+
+    template <typename T>
+    blitz::TinyVector<T, N> grad(const blitz::TinyVector<T, N> &x) const
+    {
+        return blitz::TinyVector<T, N>(2.0 * (x(0)-2.0), 2.0*(x(1)-2.0));
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -74,66 +105,105 @@ int main(int argc, char *argv[])
     /// evaluate levelset and it's gradient
     Algoim::LevelSet<N> phi(Xc, nor, rho, delta);
     TinyVector<double, N> x;
-    x(0) = 4;
-    x(1) = 0;
+    x(0) = 2;
+    x(1) = 0.844;
+    std::cout << std::setprecision(10) << std::endl;
     cout << "phi " << phi(x) << endl;
     cout << "grad phi " << phi.grad(x) << endl;
-    std::cout << std::setprecision(10) << std::endl;
-    
-    //"Area of a 2D circle using automatic subdivision
-    // {
-    //     std::cout << "Area of a 2D circle using automatic subdivision:\n";
-    //     blitz::TinyVector<double, N> xupper;
-    //     blitz::TinyVector<double, N> xlower;
-    //     xlower = {0.9, 0.9};
-    //     xupper = {3.1, 3.1};
-    //     auto q = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double,2>(xlower, xupper), -1, -1, 2);
-    //     double area = q([](const TinyVector<double,2> x) { return 1.0; });
-    //     std::cout << "  computed area = " << area << "\n";
-    //     std::cout << "    (exact area = "  <<  M_PI << "\n";
-    // }
+    cout << "hessian phi " << phi.hessian(x) << endl;
+    cout << " ------------------------------------------- " << endl;
 
-    // /// Area of a 2D ellipse, computed via the cells of a Cartesian grid
+
+    /// Area of a 2D ellipse, computed via the cells of a Cartesian grid
     // {
     //     int n = 64;
-    //     std::cout << "Area of a 2D circle, computed via the cells of a " << n << " by " << n << " Cartesian grid:\n";
-    //     double dx = 2.2 / n;
+    //     std::cout << "Area of a 2D ellipse, computed via the cells of a " << n << " by " << n << " Cartesian grid:\n";
+    //     double dx = 8.2 / n;
     //     double dy = 2.2 / n;
-    //     double min_x = 0.9;
-    //     double min_y = 0.9;
+    //     double min_x = -4.1;
+    //     double min_y = -1.1;
     //     double area = 0.0;
     //     for (int i = 0; i < n; ++i)
     //         for (int j = 0; j < n; ++j)
     //         {
     //             blitz::TinyVector<double, 2> xmin = {min_x + i * dx, min_y + j * dy};
     //             blitz::TinyVector<double, 2> xmax = {min_x + i * dx + dx, min_y + j * dy + dy};
-    //             cout << "xmin " << xmin  << "     " << "xmax " << xmax << endl;
+    //             //cout << "xmin " << xmin  << "     " << "xmax " << xmax << endl;
     //             auto q = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double, 2>(xmin, xmax), -1, -1, 4);
     //             area += q([](TinyVector<double, 2> x) { return 1.0; });
     //         }
     //     std::cout << "  computed area = " << area << "\n";
-    //     std::cout << "    exact area = " << M_PI << "\n";
+    //     std::cout << "    exact area = " << 4.0 * M_PI << "\n";
     // }
-     /// Area of a 2D ellipse, computed via the cells of a Cartesian grid
+    #if 0
+    //"Area of a 2D circle using automatic subdivision
     {
-        int n = 64;
-        std::cout << "Area of a 2D ellipse, computed via the cells of a " << n << " by " << n << " Cartesian grid:\n";
-        double dx = 8.2 / n;
-        double dy = 2.2 / n;
-        double min_x = -4.1;
-        double min_y = -1.1;
-        double area = 0.0;
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-            {
-                blitz::TinyVector<double, 2> xmin = {min_x + i * dx, min_y + j * dy};
-                blitz::TinyVector<double, 2> xmax = {min_x + i * dx + dx, min_y + j * dy + dy};
-                //cout << "xmin " << xmin  << "     " << "xmax " << xmax << endl;
-                auto q = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double, 2>(xmin, xmax), -1, -1, 4);
-                area += q([](TinyVector<double, 2> x) { return 1.0; });
-            }
+        std::cout << "Area of a 2D circle using automatic subdivision:\n";
+        blitz::TinyVector<double, N> xupper;
+        blitz::TinyVector<double, N> xlower;
+        xlower = {0.9, 0.9};
+        xupper = {3.1, 3.1};
+        auto q = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double, 2>(xlower, xupper), -1, -1, 2);
+        double area = q([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed area = " << area << "\n";
+        std::cout << "    (exact area = " <<  M_PI << "\n";
+        auto qp = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double, 2>(xlower, xupper), 2, -1, 2);
+        double peri = qp([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed perimeter = " << peri << "\n";
+        std::cout << "    (exact perimeter = " <<  2.0*M_PI << "\n";
+    }
+    //"Area of a 2D ellipse using automatic subdivision
+    {
+        Circle<2> phi2;
+        std::cout << "Area of a 2D circle using (exact level-set) automatic subdivision:\n";
+        blitz::TinyVector<double, N> xupper;
+        blitz::TinyVector<double, N> xlower;
+        xlower = {0.9, 0.9};
+        xupper = {3.1, 3.1};
+        auto q = Algoim::quadGen<2>(phi2, Algoim::BoundingBox<double, 2>(xlower, xupper), -1, -1, 2);
+        double area = q([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed area = " << area << "\n";
+        std::cout << "    (exact area = " <<  M_PI << "\n";
+        auto qp = Algoim::quadGen<2>(phi2, Algoim::BoundingBox<double, 2>(xlower, xupper), 2, -1, 2);
+        double peri = qp([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed perimeter = " << peri << "\n";
+        std::cout << "    (exact perimeter = " <<  2.0*M_PI << "\n";
+    }
+    #endif
+
+    //"Area of a 2D ellipse using automatic subdivision
+    {
+        std::cout << "Area of a 2D ellipse using automatic subdivision:\n";
+        blitz::TinyVector<double, N> xupper;
+        blitz::TinyVector<double, N> xlower;
+        xlower = {-4.1, -1.1};
+        xupper = {4.1, 1.1};
+        auto q = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double, 2>(xlower, xupper), -1, -1, 4);
+        double area = q([](const TinyVector<double, 2> x) { return 1.0; });
         std::cout << "  computed area = " << area << "\n";
         std::cout << "    exact area = " << 4.0 * M_PI << "\n";
+        auto qp = Algoim::quadGen<2>(phi, Algoim::BoundingBox<double, 2>(xlower, xupper), 2, -1, 4);
+        double peri = qp([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed perimeter = " << peri << "\n";
+        std::cout << "    exact perimeter = " <<  17.15684 << "\n";
+    }
+
+    //"Area of a 2D ellipse using automatic subdivision
+    {
+        Ellipsoid<2> phi2;
+        std::cout << "Area of a 2D ellipse using (exact level-set) automatic subdivision:\n";
+        blitz::TinyVector<double, N> xupper;
+        blitz::TinyVector<double, N> xlower;
+        xlower = {-4.1, -1.1};
+        xupper = {4.1, 1.1};
+        auto q = Algoim::quadGen<2>(phi2, Algoim::BoundingBox<double, 2>(xlower, xupper), -1, -1, 4);
+        double area = q([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed area = " << area << "\n";
+        std::cout << "    exact area = " << 4.0 * M_PI << "\n";
+        auto qp = Algoim::quadGen<2>(phi2, Algoim::BoundingBox<double, 2>(xlower, xupper), 2, -1, 4);
+        double peri = qp([](const TinyVector<double, 2> x) { return 1.0; });
+        std::cout << "  computed perimeter = " << peri << "\n";
+        std::cout << "    exact perimeter = " <<  17.15684 << "\n";
     }
 
     return 0;
--- src/algoim_levelset.hpp
+++ src/algoim_levelset.hpp
@@ -119,7 +119,102 @@ namespace Algoim
             //cout << "phi_bar " << phi_bar << endl;
             return phi_bar;
         }
-#if 0
+
+        /// calculate the second derivative of level-set function
+        /// \param[in] x - tinyvector of point where gradphi(x) needs to be calculated
+        /// \param[out] phi_xx - level-set function's second derivative
+        TinyVector<double, N + 1> hessian(const blitz::TinyVector<double, N> &x) const
+        {
+            using std::exp;
+            using std::sqrt;
+            int nbnd = xbnd.size();
+            TinyVector<double, N> phi_xx;
+            TinyVector<double, N + 1> phix_bar;
+            /// find minimum distance
+            double min_dist = 1e+100;
+            for (int i = 0; i < nbnd; ++i)
+            {
+                TinyVector<double, N> x_diff;
+                x_diff = x - xbnd.at(i);
+                double deli_x = sqrt(magsqr(x_diff) + delta);
+                min_dist = min(min_dist, deli_x);
+            }
+            double numer = 0.0;
+            double denom = 0.0;
+            for (int i = 0; i < nbnd; ++i)
+            {
+                TinyVector<double, N> x_diff;
+                x_diff = x - xbnd.at(i);
+                double perp = dot(x_diff, norm.at(i));
+                double delx = sqrt(magsqr(x_diff) + delta);
+                double expc = exp(-rho * (delx - min_dist));
+                denom += expc;
+                numer += perp * expc;
+            }
+            TinyVector<double, N> numer_x = 0.0;
+            TinyVector<double, N> numer_xx = 0.0;
+            TinyVector<double, N> denom_x = 0.0;
+            TinyVector<double, N> denom_xx = 0.0;
+            double numer_xy = 0.0;
+            double denom_xy = 0.0;
+            double term1;
+            double term2;
+            for (int i = 0; i < nbnd; ++i)
+            {
+                TinyVector<double, N> x_diff;
+                x_diff = x - xbnd.at(i);
+                double perp = dot(x_diff, norm.at(i));
+                double delx = sqrt(magsqr(x_diff) + delta);
+                double expc = exp(-rho * (delx - min_dist));
+                for (int d = 0; d < N; ++d)
+                {
+                    double expc_x = rho * expc * x_diff(d) / delx;
+                    double perp_bar = norm.at(i)(d);
+                    numer_x(d) += (perp * expc_x) + (expc * perp_bar);
+                    denom_x(d) += expc_x;
+                    term1 = delx * (expc_x * x_diff(d)) + expc;
+                    term2 = expc * (x_diff(d) * x_diff(d)) / delx;
+                    double expc_xx = rho * (term1 + term2) / (delx * delx);
+
+                    /// this is zero for linear case
+                    double perp_xx = 0.0;
+                    term1 = (perp * expc_xx) + (2.0 * perp_bar * expc_x);
+                    term2 = expc * perp_xx;
+                    numer_xx(d) += term1 + term2;
+                    denom_xx(d) += expc_xx;
+                }
+
+                double expc_x = rho * expc * x_diff(0) / delx;
+                double expc_y = rho * expc * x_diff(1) / delx;
+                double perp_x = norm.at(i)(0);
+                double perp_y = norm.at(i)(1);
+                term1 = delx * expc_y * x_diff(0);
+                term2 = expc * x_diff(0) * x_diff(1) / delx;
+                double expc_xy = rho * (term1 + term2) / (delx * delx);
+                double perp_xy = 0.0;
+                term1 = (perp * expc_xy) + (expc_x * perp_y);
+                term2 = (expc_y * perp_x) + (perp_xy * expc);
+                numer_xy += term1 + term2;
+                denom_xy += expc_xy;
+            }
+            for (int d = 0; d < N; ++d)
+            {
+                term1 = (denom * numer_xx(d)) - (numer * denom_xx(d));
+                term2 = (denom * numer_x(d)) - (numer * denom_x(d));
+                double fac = denom * denom;
+                phi_xx(d) = term1 / fac - (2.0 * term2 * denom_x(d) / (fac * denom));
+            }
+            term1 = (denom * numer_xy) + (numer_x(0) * denom_x(1));
+            term2 = (numer * denom_xy) + (denom_x(0) * numer_x(1));
+            double term3 = ((denom * numer_x(0)) - (numer * denom_x(0))) * 2.0 * denom_x(1);
+            double fac = denom * denom;
+            double phi_xy = (term1 - term2) / fac;
+            phi_xy -= term3 / (fac * denom);
+            phix_bar(0) = phi_xx(0);
+            phix_bar(1) = phi_xy;
+            phix_bar(2) = phi_xx(1);
+            return phix_bar;
+        }
         /// calculate the level-set function value (here the data type is interval)
         /// \param[in] x - tinyvector of point where phi(x) needs to be calculated
         /// \param[out] phi - level-set function value
@@ -207,8 +302,135 @@ namespace Algoim
             Interval<N> phi = Interval<N>(phi_xc, beta, eps);
             return phi;
         }
-#endif
-#if 1
+        
+        /// calculate the gradient of level-set function
+        /// \param[in] x - tinyvector of point where gradphi(x) needs to be calculated
+        /// \param[out] phi_bar - level-set function gradient value
+        TinyVector<Interval<N>, N> grad(const blitz::TinyVector<Interval<N>, N> &x) const
+        {
+            using std::exp;
+            using std::sqrt;
+            TinyVector<double, N> xc;
+            xc(0) = x(0).alpha;
+            xc(1) = x(1).alpha;
+            int nbnd = xbnd.size();
+            /// find minimum distance
+            double min_dist = 1e+100;
+            int j_max = 0;
+            for (int i = 0; i < nbnd; ++i)
+            {
+                TinyVector<double, N> x_diff;
+                x_diff = xc - xbnd.at(i);
+                double deli_x = sqrt(magsqr(x_diff) + delta);
+                // min_dist = min(min_dist, deli_x);
+                if (deli_x < min_dist)
+                {
+                    min_dist = deli_x;
+                    j_max = i;
+                }
+            }
+            double denom = 0.0;
+            /// evaluate the level-set
+            for (int i = 0; i < nbnd; ++i)
+            {
+                TinyVector<double, N> x_diff;
+                x_diff = xc - xbnd.at(i);
+                double dist = dot(x_diff, norm.at(i));
+                double delx = sqrt(magsqr(x_diff) + delta);
+                double expc = exp(-rho * (delx - min_dist));
+                denom += expc;
+            }
+            /// calculate levelset derivative bounds
+            TinyVector<double, N> xmin;
+            TinyVector<double, N> xmax;
+            xmax = xc + x(0).delta();
+            xmin = xc - x(0).delta();
+            const int nv = pow(2, N);
+
+            TinyVector<TinyVector<double, N>, nv> vert;
+            // \Note- need to modify this
+            vert(0) = xmin;
+            vert(1) = {xmax(0), xmin(1)};
+            vert(2) = xmax;
+            vert(3) = {xmin(0), xmax(1)};
+
+            double djmin = 1e100;
+            double djmax = -1e100;
+            for (int k = 0; k < nv; ++k)
+            {
+                TinyVector<double, N> ds;
+                ds = vert(k) - xbnd.at(j_max);
+                double perp = dot(ds, norm.at(j_max));
+                djmin = min(djmin, perp);
+                djmax = max(djmax, perp);
+            }
+            TinyVector<double, N> dx = x(0).delta();
+            double L = sqrt(magsqr(dx));
+            LevelSet<N> ls(xbnd, norm, rho, delta);
+            TinyVector<double, N> beta = ls.grad(xc);
+            double delx_phi = beta(0);
+            double dely_phi = beta(1);
+            /// find `j` index at which psi(xc) is largest
+            double phix_bnd = 0.0;
+            double phiy_bnd = 0.0;
+            for (int j = 0; j < nbnd; ++j)
+            {
+                double delx_perp = norm.at(j)(0);
+                double dely_perp = norm.at(j)(1);
+                TinyVector<double, N> x_diff;
+                x_diff = xc - xbnd.at(j);
+                double deli_x = sqrt(magsqr(x_diff) + delta);
+                double expc = exp(-rho * (deli_x - min_dist));
+                double psi_xc = expc / denom;
+                double bar_psi = min(1.0, psi_xc * exp(2 * rho * L));
+                phix_bnd += abs(delx_perp - delx_phi) * bar_psi;
+                phiy_bnd += abs(dely_perp - dely_phi) * bar_psi;
+                if (j != j_max)
+                {
+                    double dmin = 1e100;
+                    double dmax = -1e100;
+                    double psi_bar = psi_xc * exp(-2 * rho * L);
+                    for (int k = 0; k < nv; ++k)
+                    {
+                        TinyVector<double, N> ds;
+                        ds = vert(k) - xbnd.at(j);
+                        double perp = dot(ds, norm.at(j));
+                        dmin = min(dmin, perp);
+                        dmax = max(dmax, perp);
+                    }
+                    double min_psi = min(bar_psi * (1 - psi_bar), 0.25);
+                    double temp;
+                    if ((bar_psi > 0.5) && (psi_bar < 0.5))
+                    {
+                        temp = 2.0 * rho * 0.25;
+                    }
+                    else
+                    {
+                        temp = 2.0 * rho * max(psi_bar * (1.0 - psi_bar), bar_psi * (1.0 - bar_psi));
+                    }
+                    phix_bnd += max(dmax - djmin, -dmin + djmax) * temp;
+                    phiy_bnd += max(dmax - djmin, -dmin + djmax) * 2 * rho * min_psi;
+                }
+            }
+            double eps_x = phix_bnd;
+            double eps_y = phiy_bnd;
+            TinyVector<double, N + 1> hes;
+            hes = hessian(xc);
+            TinyVector<double, N> beta_x, beta_y;
+            beta_x(0) = hes(0);
+            beta_x(1) = hes(1);
+            beta_y(0) = hes(1);
+            beta_y(1) = hes(2);
+            for (int dim = 0; dim < N; ++dim)
+            {
+                eps_x -= std::abs(beta_x(dim)) * x(0).delta(dim);
+                eps_y -= std::abs(beta_y(dim)) * x(0).delta(dim);
+            }
+            Interval<N> phi_x = Interval<N>(delx_phi, beta_x, eps_x);
+            Interval<N> phi_y = Interval<N>(dely_phi, beta_y, eps_y);
+            return blitz::TinyVector<Interval<N>, N>(phi_x, phi_y);
+        }
+#if 0
         /// This checks the level-set function along a given x slice
         /// \param[in] x - tinyvector of point where phi(x) needs to be calculated
         /// \param[out] phi - level-set function value
@@ -397,112 +619,7 @@ namespace Algoim
         }
 #endif
 
-        /// calculate the gradient of level-set function
-        /// \param[in] x - tinyvector of point where gradphi(x) needs to be calculated
-        /// \param[out] phi_bar - level-set function gradient value
-        TinyVector<Interval<N>, N> grad(const blitz::TinyVector<Interval<N>, N> &x) const
-        {
 #if 0
-        using std::exp;
-        using std::sqrt;
-        TinyVector<double, N> xc;
-        xc(0) = x(0).alpha;
-        xc(1) = x(1).alpha;
-        int nbnd = xbnd.size();
-        /// find minimum distance
-        double min_dist = 1e+100;
-        int j_max = 0;
-        for (int i = 0; i < nbnd; ++i)
-        {
-            TinyVector<double, N> x_diff;
-            x_diff = xc - xbnd.at(i);
-            double deli_x = sqrt(magsqr(x_diff) + delta);
-            // min_dist = min(min_dist, deli_x);
-            if (deli_x < min_dist)
-            {
-                min_dist = deli_x;
-                j_max = i;
-            }
-        }
-        double denom = 0.0;
-        /// evaluate the level-set
-        for (int i = 0; i < nbnd; ++i)
-        {
-            TinyVector<double, N> x_diff;
-            x_diff = xc - xbnd.at(i);
-            double dist = dot(x_diff, norm.at(i));
-            double delx = sqrt(magsqr(x_diff) + delta);
-            double expc = exp(-rho * (delx - min_dist));
-            denom += expc;
-        }
-        /// calculate levelset derivative bounds
-        TinyVector<double, N> xmin;
-        TinyVector<double, N> xmax;
-        xmax = xc + x(0).delta();
-        xmin = xc - x(0).delta();
-        const int nv = pow(2, N);
-
-        TinyVector<TinyVector<double, N>, nv> vert;
-        // \Note- need to modify this
-        vert(0) = xmin;
-        vert(1) = {xmax(0), xmin(1)};
-        vert(2) = xmax;
-        vert(3) = {xmin(0), xmax(1)};
-
-        double djmin = 1e100;
-        double djmax = -1e100;
-        for (int k = 0; k < nv; ++k)
-        {
-            TinyVector<double, N> ds;
-            ds = vert(k) - xbnd.at(j_max);
-            double perp = dot(ds, norm.at(j_max));
-            djmin = min(djmin, perp);
-            djmax = max(djmax, perp);
-        }
-        TinyVector<double, N> dx = x(0).delta();
-        double L = sqrt(magsqr(dx));
-        LevelSet<N> ls(xbnd, norm, rho, delta);
-        TinyVector<double, N> beta = ls.grad(xc);
-        double delx_phi = beta(0);
-        double dely_phi = beta(1);
-        /// find `j` index at which psi(xc) is largest
-        double phix_bnd = 0.0;
-        double phiy_bnd = 0.0;
-        for (int j = 0; j < nbnd; ++j)
-        {
-            double delx_perp = norm.at(j)(0);
-            double dely_perp = norm.at(j)(1);
-            TinyVector<double, N> x_diff;
-            x_diff = xc - xbnd.at(j);
-            double deli_x = sqrt(magsqr(x_diff) + delta);
-            double expc = exp(-rho * (deli_x - min_dist));
-            double psi_xc = expc / denom;
-            double bar_psi = min(1.0, psi_xc * exp(2 * rho * L));
-            phix_bnd += abs(delx_perp - delx_phi) * bar_psi;
-            phiy_bnd += abs(dely_perp - dely_phi) * bar_psi;
-            if (j != j_max)
-            {
-                double dmin = 1e100;
-                double dmax = -1e100;
-                double psi_bar = psi_xc * exp(-2 * rho * L);
-                for (int k = 0; k < nv; ++k)
-                {
-                    TinyVector<double, N> ds;
-                    ds = vert(k) - xbnd.at(j);
-                    double perp = dot(ds, norm.at(j));
-                    dmin = min(dmin, perp);
-                    dmax = max(dmax, perp);
-                }
-                double min_psi = min(bar_psi * (1 - psi_bar), 0.25);
-                phix_bnd += max(dmax - djmin, -dmin + djmax) * 2 * rho * min_psi;
-                phiy_bnd += max(dmax - djmin, -dmin + djmax) * 2 * rho * min_psi;
-            }
-        }
-        Interval<N> phi_x = Interval<N>(0);
-        Interval<N> phi_y = Interval<N>(0);
-        return blitz::TinyVector<Interval<N>, N>(phi_x, phi_y);
-#endif
-#if 1
             int nbnd = xbnd.size();
             /// find minimum distance
             double min_dist = 0.0;
@@ -564,7 +681,6 @@ namespace Algoim
             }
             return blitz::TinyVector<Interval<N>, N>(phi_x, phi_y);
 #endif
-        }
 
 /// Just checking if the Interval type functions work correctly using double
 #if 0
